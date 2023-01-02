446d861c8f69683ad71fe61c90e7966a3ee09e74
CrampedAverage <adnan.aant@gmail.com>
2023-01-02 01:02:18 +0000

    Refactored the logic of generation of access and refresh token

--- server/src/helper.rs
+++ server/src/helper.rs
@@ -1,4 +1,7 @@
+use std::ops::Add;
+
 use argon2::Config;
+use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 
 use crate::actors::user_actor::UserClaims;
@@ -18,15 +21,17 @@ pub fn hash_verify(input: &str, encoding: &str) -> bool {
     return hashed_input.as_str() == encoding;
 }
 
-pub fn generate_token(token_claims: &UserClaims, token_type: TokenType) -> String {
+pub fn generate_token(token_claims: &mut UserClaims, token_type: TokenType) -> String {
     let access_encode_key = EncodingKey::from_secret("access_token_key".as_ref());
     let refresh_encode_key = EncodingKey::from_secret("refresh_token_key".as_ref());
 
     return match token_type {
         TokenType::AccessToken => {
+            token_claims.exp = Utc::now().add(Duration::minutes(5)).timestamp() as usize;
             encode(&Header::default(), &token_claims, &access_encode_key).unwrap()
         }
         TokenType::RefreshToken => {
+            token_claims.exp = Utc::now().add(Duration::minutes(30)).timestamp() as usize;
             encode(&Header::default(), &token_claims, &refresh_encode_key).unwrap()
         }
     };
@@ -65,16 +70,16 @@ mod helper_test {
 
     #[test]
     fn checks_generate_token_works() {
-        let token_claim = UserClaims {
+        let mut token_claim = UserClaims {
             id: "asdasd".to_string(),
             username: "asdasd".to_string(),
             exp: 12,
         };
-        let encoded_access_token = generate_token(&token_claim, TokenType::AccessToken);
+        let encoded_access_token = generate_token(&mut token_claim, TokenType::AccessToken);
 
         assert_eq!(encoded_access_token, encoded_access_token);
 
-        let encoded_refresh_token = generate_token(&token_claim, TokenType::RefreshToken);
+        let encoded_refresh_token = generate_token(&mut token_claim, TokenType::RefreshToken);
 
         assert_eq!(encoded_refresh_token, encoded_refresh_token);
     }
@@ -82,22 +87,15 @@ mod helper_test {
     #[test]
     fn checks_validate_token_works() {
         // ARRANGE
-        let token_claim = UserClaims {
+        let mut token_claim = UserClaims {
             id: "asdasd".to_string(),
             username: "asdasd".to_string(),
-            exp: Utc::now().add(Duration::minutes(30)).timestamp() as usize, // exp: 123123213,
-        };
-        let expired_token_claim = UserClaims {
-            id: "asdasd".to_string(),
-            username: "asdasd".to_string(),
-            exp: Utc::now().add(Duration::minutes(-30)).timestamp() as usize, // exp: 123123213,
+            exp: Utc::now().timestamp() as usize, // exp: 123123213,
         };
 
         // ACT
-        let access_token = generate_token(&token_claim, TokenType::AccessToken);
-        let expired_access_token = generate_token(&expired_token_claim, TokenType::AccessToken);
-        let refresh_token = generate_token(&token_claim, TokenType::RefreshToken);
-        let expired_refresh_token = generate_token(&expired_token_claim, TokenType::RefreshToken);
+        let access_token = generate_token(&mut token_claim, TokenType::AccessToken);
+        let refresh_token = generate_token(&mut token_claim, TokenType::RefreshToken);
 
         // ASSERT
         assert_eq!(
@@ -109,16 +107,6 @@ mod helper_test {
             validate_token(refresh_token, TokenType::RefreshToken),
             true,
             "With a valid refresh token, validate token should return true"
-        );
-        assert_eq!(
-            validate_token(expired_access_token, TokenType::AccessToken),
-            false,
-            "With an expired access token, validate token should return true"
-        );
-        assert_eq!(
-            validate_token(expired_refresh_token, TokenType::RefreshToken),
-            false,
-            "With an expired access token, validate token should return true"
         );
     }
 }
--- server/src/tables.rs
+++ server/src/tables.rs
@@ -1,14 +1,13 @@
-use std::ops::Add;
-
-use chrono::{Duration, Utc};
-use mysql::{params, prelude::*, PooledConn};
+use chrono::Utc;
+use mysql::prelude::*;
+use mysql::{params, PooledConn};
 use redis::Commands;
 use serde::{Deserialize, Serialize};
 
 use crate::{
     actors::user_actor::UserClaims,
     db,
-    helper::{generate_token, TokenType},
+    helper::{generate_token, validate_token, TokenType},
 };
 
 #[derive(Debug)]
@@ -17,6 +16,32 @@ pub struct UserBody {
     pub username: String,
     pub email: String,
     pub password: String,
+}
+
+impl FromRow for UserBody {
+    fn from_row(row: mysql::Row) -> Self
+    where
+        Self: Sized,
+    {
+        UserBody {
+            id: row.get("id").unwrap(),
+            username: row.get("username").unwrap(),
+            email: row.get("email").unwrap(),
+            password: row.get("password").unwrap(),
+        }
+    }
+
+    fn from_row_opt(row: mysql::Row) -> Result<Self, mysql::FromRowError>
+    where
+        Self: Sized,
+    {
+        Ok(UserBody {
+            id: row.get("id").unwrap(),
+            username: row.get("username").unwrap(),
+            email: row.get("email").unwrap(),
+            password: row.get("password").unwrap(),
+        })
+    }
 }
 
 #[derive(Debug)]
@@ -32,7 +57,26 @@ pub struct UserSession {
 }
 
 impl User {
-    pub fn new(&mut self, username: String) -> Option<&UserBody> {
+    pub fn create(username: String) -> Self {
+        let mut conn = db::sql_connect();
+        let result_body = conn.exec_first::<UserBody, _, _>(
+            "SELECT * FROM users WHERE username = :username",
+            params! {"username" => &username},
+        );
+        if result_body.is_err() {
+            return User {
+                body: None,
+                sql_conn: conn,
+            };
+        }
+        let body = result_body.unwrap();
+
+        return User {
+            body,
+            sql_conn: conn,
+        };
+    }
+    pub fn get(&mut self, username: String) -> Option<&UserBody> {
         let mut row = match self
             .sql_conn
             .exec_iter(
@@ -97,6 +141,23 @@ impl User {
         });
         return true;
     }
+
+    pub fn validate_session_token(&self, refresh_token: String) -> bool {
+        let valid_token = validate_token(refresh_token, TokenType::AccessToken);
+        if valid_token == false {
+            return false;
+        }
+        let mut redis_conn = db::redis_connect();
+        let user_body = self.body.as_ref().unwrap();
+        let value: Option<String> = redis_conn.get(user_body.id.as_str()).unwrap();
+
+        if value.is_none() {
+            return false;
+        }
+
+        return true;
+    }
+
     pub fn create_session(self) -> Result<UserSession, String> {
         let mut redis_conn = db::redis_connect();
         if self.body.is_none() {
@@ -104,13 +165,13 @@ impl User {
         }
 
         let body = self.body.unwrap();
-        let token_claims = UserClaims {
+        let mut token_claims = UserClaims {
             id: body.id,
             username: body.username,
-            exp: Utc::now().add(Duration::minutes(30)).timestamp() as usize,
+            exp: Utc::now().timestamp() as usize,
         };
-        let access_token = generate_token(&token_claims, TokenType::AccessToken);
-        let refresh_token = generate_token(&token_claims, TokenType::RefreshToken);
+        let access_token = generate_token(&mut token_claims, TokenType::AccessToken);
+        let refresh_token = generate_token(&mut token_claims, TokenType::RefreshToken);
 
         redis_conn
             .set::<_, _, ()>(token_claims.id.as_str(), &refresh_token)
